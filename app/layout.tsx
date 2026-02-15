a246d75fa879fa57cc99473cd2095756ba01ec70
nakakoshi <nakakoshi3100@gmail.com>
2026-02-16 02:07:26 +0900

    グーグルタグマネージャー実装（GTM）

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
+import Script from "next/script";
 import { ScrollRevealInit } from "@/components/ScrollRevealInit";
 import "./globals.css";
+
+const GTM_ID = "GTM-PB64MRCV";
 
 const notoSansJP = Noto_Sans_JP({
   variable: "--font-noto-sans",
@@ -39,9 +42,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+      </head>
       <body
         className={`${notoSansJP.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ScrollRevealInit />
         {children}
       </body>
